c41c4ddd2466949487b50214bb6a1d884f8e23a5
Maurice Martinez <MNM-Nick@users.noreply.github.com>
2022-06-06 17:35:47 +0000

    learning about input fucntion

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+name= input("What is your name?\n>")
+date= input("What day is today?\n>")
+print("Awesome",name+", what a lovely",date,"it is!")
+import time
+time.sleep(5)
+print ("sike, I don't like",date+"s!! BWUAHAHAH")
+time.sleep(2)
+print("loser....")
